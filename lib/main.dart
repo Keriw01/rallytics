b75805f3a7128d59a5f856d2bc893116693f7c7a
Keriw01 <sebas342@o2.pl>
2025-07-25 20:25:13 +0200

    fix(ci): correct creation of firebase_options.dart in build workflow, fix file formatting issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:rallytics/app/app.dart';
 import 'package:rallytics/core/di/injection.dart';
-import 'package:rallytics/firebase_options.dart';
+import 'package:rallytics/core/config/firebase_config.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await configureDependencies();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseConfig.initialize();
 
   runApp(const App());
 }
