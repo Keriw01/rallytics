748fa5537bca25118b6fb936ab4c76e7a748f8aa
Keriw01 <sebas342@o2.pl>
2025-07-25 20:11:53 +0200

    fix: update locale from 'en' to 'pl' in intl_pl.arb

--- lib/generated/intl/messages_pl.dart
+++ lib/generated/intl/messages_pl.dart
@@ -1,5 +1,5 @@
 // DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
-// This is a library that provides messages for a en locale. All the
+// This is a library that provides messages for a pl locale. All the
 // messages from the main program should be duplicated here with the same
 // function name.
 
@@ -18,41 +18,42 @@ final messages = new MessageLookup();
 typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 
 class MessageLookup extends MessageLookupByLibrary {
-  String get localeName => 'en';
+  String get localeName => 'pl';
 
-  static String m0(userName) => "Welcome back, ${userName}!";
+  static String m0(userName) => "Witaj z powrotem, ${userName}!";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "appTitle": MessageLookupByLibrary.simpleMessage("Rallytics"),
-        "cancelButton": MessageLookupByLibrary.simpleMessage("Cancel"),
+        "cancelButton": MessageLookupByLibrary.simpleMessage("Anuluj"),
         "errorEmailInUse": MessageLookupByLibrary.simpleMessage(
-            "This email is already in use. Please try another one."),
+            "Ten email jest już używany. Spróbuj innego."),
         "errorGeneric": MessageLookupByLibrary.simpleMessage(
-            "An unexpected error occurred. Please try again later."),
-        "errorInvalidCredentials":
-            MessageLookupByLibrary.simpleMessage("Invalid email or password."),
+            "Nie oczekiwany błąd wystąpił. Spróbuj ponownie później."),
+        "errorInvalidCredentials": MessageLookupByLibrary.simpleMessage(
+            "Nie prawidłowy email lub hasło."),
         "errorWeakPassword":
-            MessageLookupByLibrary.simpleMessage("The password is too weak."),
+            MessageLookupByLibrary.simpleMessage("Hasło jest zbyt słabe."),
         "liveScoresMatchStatusFinished":
-            MessageLookupByLibrary.simpleMessage("Finished"),
+            MessageLookupByLibrary.simpleMessage("Zakończone"),
         "liveScoresMatchStatusUpcoming":
-            MessageLookupByLibrary.simpleMessage("Upcoming"),
-        "liveScoresTitle": MessageLookupByLibrary.simpleMessage("Live Scores"),
-        "loginButton": MessageLookupByLibrary.simpleMessage("Log In"),
+            MessageLookupByLibrary.simpleMessage("Nadchodzące"),
+        "liveScoresTitle":
+            MessageLookupByLibrary.simpleMessage("Wyniki na Żywo"),
+        "loginButton": MessageLookupByLibrary.simpleMessage("Zaloguj się"),
         "loginEmailLabel": MessageLookupByLibrary.simpleMessage("Email"),
         "loginErrorInvalidCredentials": MessageLookupByLibrary.simpleMessage(
-            "Invalid email or password. Please try again."),
-        "loginPasswordHint": MessageLookupByLibrary.simpleMessage("Password"),
-        "loginTitle": MessageLookupByLibrary.simpleMessage("Sign In"),
-        "logoutButton": MessageLookupByLibrary.simpleMessage("Log Out"),
-        "saveButton": MessageLookupByLibrary.simpleMessage("Save"),
+            "Nie prawidłowy email lub hasło. Spróbuj ponownie."),
+        "loginPasswordHint": MessageLookupByLibrary.simpleMessage("Hasło"),
+        "loginTitle": MessageLookupByLibrary.simpleMessage("Logowanie"),
+        "logoutButton": MessageLookupByLibrary.simpleMessage("Wyloguj się"),
+        "saveButton": MessageLookupByLibrary.simpleMessage("Zapisz"),
         "trainingSessionImportButton":
-            MessageLookupByLibrary.simpleMessage("Import from Gallery"),
+            MessageLookupByLibrary.simpleMessage("Importuj z Galerii"),
         "trainingSessionStartButton":
-            MessageLookupByLibrary.simpleMessage("Start Recording"),
+            MessageLookupByLibrary.simpleMessage("Zacznij Nagrywanie"),
         "trainingSessionTitle":
-            MessageLookupByLibrary.simpleMessage("New Session"),
+            MessageLookupByLibrary.simpleMessage("Nowa Sesja"),
         "welcomeUserMessage": m0
       };
 }
